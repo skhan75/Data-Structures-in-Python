a7b08fc0b43d5840387f36493f66a38f0e74faf9
Sami Ahmad Khan <skhan75@hawk.iit.edu>
2017-03-14 21:19:22 -0500

    comments added

--- Sort/CountSort.py
+++ Sort/CountSort.py
@@ -1,6 +1,8 @@
 """Counting sort is a sorting technique based on keys between a specific range.
 It works by counting the number of objects having distinct key values (kind of hashing).
-Then doing some arithmetic to calculate the position of each object in the output sequence"""
+Then doing some arithmetic to calculate the position of each object in the output sequence
+
+* Works for both integer and string inputs (both seperately and combined"""
 
 # RUNTIME --> O(n+k); where, n --> no of elements
 # and k --> range of input
